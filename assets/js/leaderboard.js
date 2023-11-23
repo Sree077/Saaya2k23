92edd2cec0aeab6722186605213f964b1e1b01ad
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2023-11-23 09:56:57 +0530

    modified

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,18 +1,17 @@
 function Click() {
-    fetch('https://saaya23dashborad.onrender.com/getPoint')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); // Assuming the response is in JSON format
-      })
-      .then(data => {
-        // Handle the data from the API
-        console.log(data);
-      })
-      .catch(error => {
-        // Handle errors during the fetch
-        console.error('There was a problem with the fetch operation:', error);
-      });
-  };
-  
+  fetch('https://saaya23dashborad.onrender.com/getPoint')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Assuming the response is in JSON format
+    })
+    .then(data => {
+      // Handle the data from the API
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle errors during the fetch
+      console.error('There was a problem with the fetch operation:', error);
+    });
+};
